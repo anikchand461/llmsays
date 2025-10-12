9437978542ad98af00d77d0a0adbb9d5757c800e
Abhiraj Adhikary <abhirajadhikary06@gmail.com>
2025-10-12 19:14:05 +0530

    added commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     package_dir={"": "src"},
-    packages, packages=find_packages(where="src"),
+    packages=find_packages(where="src"),
     python_requires=">=3.9",
     install_requires=[
         "openai>=1.40.0",
